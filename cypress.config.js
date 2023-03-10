5f3a5f962e363861e9db2dec213fe15ee253b797
Maximiliano Erce <javiererce@hotmail.com>
2023-03-10 18:59:27 -0300

    a

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
   projectId: 'k6aqyv',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
