7fdd739ba01dccc2549ceb102ad957269ab935ee
Maximiliano Erce <javiererce@hotmail.com>
2023-03-20 10:50:37 -0300

    001

---
+++ cypress/e2e/APIS/Api04.cy.js
@@ -0,0 +1,10 @@
+import { beforeEach } from "mocha"
+require('@cypress/xpath');
+
+describe("Loguear -Basic Auth y Auth con Forms",function(){
+    it('Loguea usando Auth de Cypress', () => {
+        cy.visit("https://the-internet.herokuapp.com/basic_auth",{
+           
+        })
+    })
+})
