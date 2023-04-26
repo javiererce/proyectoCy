9573858947be2b74ca258333e88294cc28dc1211
Maximiliano Erce <javiererce@hotmail.com>
2023-04-26 17:07:48 -0300

    Agregando ejercicios de Apis

---
+++ cypress/e2e/API/api01.cy.js
@@ -0,0 +1,32 @@
+import { beforeEach, it } from "mocha"
+require('@cypress/xpath');
+
+describe("Ejercicio de Apis con json-server",()=>{
+    let datos;
+    it("Test Api Get 01", () => {
+        datos = cy.request("http://localhost:3000/posts")
+        datos.its("status").should("equal",200)
+    });
+    it("Test Api Get 02", () => {
+        datos = cy.request("http://localhost:3000/posts").should((response)=>{
+            expect(response.status).to.eq(200)
+        })
+    });
+    it("Test Api 03", () => {
+        cy.request({
+            method: "GET",
+            url: "http://localhost:3000/posts",
+            headers: {
+                accept: "application/json"
+            }
+        }).then(response =>{
+            let datos;
+            datos = JSON.parse(JSON.stringify(response.body))
+            expect(datos[0]).has.property("title","Ejercicio Json-Server")
+            expect(datos[0]).has.property("author","typicode")
+
+        })
+
+    });
+
+})
