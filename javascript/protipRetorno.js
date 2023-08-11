f7de5919a1b2a02d5b4c4c79acccf1a43fa014b0
Maximiliano Erce <javiererce@hotmail.com>
2023-08-10 22:57:14 -0300

    configuracion de allure

--- javascript/protipRetorno.js
+++ javascript/protipRetorno.js
@@ -1,11 +1,10 @@
 const mensaje = ()=>console.log("hola mundo")
 mensaje();
 //-------------------------------
-function imprimirArgun(){
-    console.log(arguments)
-}
-imprimirArgun(10,"javier","erce",true)
 
-const imprimirArgunto = (...arg)=>console.log(arg)
-imprimirArgunto(10,"javier","erce",true)
-//-------------------------------------------
+let nombre = "Maximiliano";
+let apellido = "Erce";
+
+let nombreCompleto = nombre +" "+ apellido;
+
+console.log(nombreCompleto);
